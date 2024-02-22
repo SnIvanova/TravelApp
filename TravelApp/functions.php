4b888af8aa9df2fe47ef6816207a75d395a227ad
Victor <victor246257@gmail.com>
2024-02-22 23:06:08 +0100

    mod

--- TravelApp/functions.php
+++ TravelApp/functions.php
@@ -37,6 +37,7 @@ function theme_scripts() {
     wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array('jquery', 'popper-js'), '5.3.0', true );
 
     wp_enqueue_script('custom-script', get_template_directory_uri() . '/assets/js/custom-script.js'); 
+    wp_enqueue_style( 'styleFooter', get_template_directory_uri().'/assets/style/styleFooter.css' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
