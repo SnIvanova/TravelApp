540a171f80e5d6414bec1caff0e7afc61d00fedd
Maria Tavino <mariatavino1@gmail.com>
2024-02-23 13:24:30 +0100

    pagina contact fatta con opzioni background

--- TravelApp/header.php
+++ TravelApp/header.php
@@ -20,8 +20,7 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    
+                <div class="collapse navbar-collapse" id="navbarNav">    
                     <?php
                         wp_nav_menu(array(
                             'theme_location' => 'primary-menu',
@@ -33,7 +32,6 @@
                             'walker' => new bootstrap_5_wp_nav_menu_walker()
                         ));
                     ?>
-
               <ul class="navbar-nav">
                     <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
