b534db80ffce75c574edcc5e53044c5f21adaefa
Victor <victor246257@gmail.com>
2024-02-26 11:43:55 +0100

    mod

---
+++ TravelApp/Home.php
@@ -0,0 +1,44 @@
+
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+
+<div id='home'>
+<?php get_header(); ?>
+<style>
+#home {
+    background: url('<?php echo get_post_meta(get_the_ID(), 'background_image', true); ?>'); 
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    height: 100vh;
+}
+
+</style>
+
+<div class="container imageBG">
+  <div class="row d-flex justify-content-center">
+    <div class="col-md-8">
+      <?php
+      if ( have_posts() ) : 
+        while ( have_posts() ) : the_post();
+          the_post_thumbnail();
+          the_title('<h1 class="card-body text-center text-white my-3">','</h1>');
+          the_content();
+        endwhile;
+      else :
+        _e('Sorry, no posts matched your criteria.', 'textdomain');
+      endif;
+      ?>
+
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
+</div>
+
+
--- TravelApp/footer.php
+++ TravelApp/footer.php
@@ -16,13 +16,13 @@
       </div>
     </div>
 
-  <div class=" container-fluid">
-    <div class="row mx-5 align-items-baseline " >
-        <div class="col-md-4 col-lg-4 text-center c1" >
-           <a href="index.php"><?php bloginfo('name'); ?></a>
+  <div class="container-fluid">
+    <div class="row  mx-5 align-items-baseline " >
+        <div class="col-md-4  col-lg-4 text-center c1" >
+           <a class="text-white" href="index.php"><?php bloginfo('name'); ?></a>
         </div>
         <div class="col-md-4 col-lg-4 text-center c2" >
-          <a class="mx-2" href="">Events</a><a class="mx-2" href="">FAQs</a><a class="mx-2" href="">The Team</a>
+          <a class="mx-2 text-white" href="">Events</a><a class="mx-2 text-white" href="">FAQs</a><a class="mx-2 text-white" href="">The Team</a>
         </div>
         <div class="col-md-4 col-lg-4 justify-content-center d-flex di" >
         <p class="mx-2" ><i class="bi bi-instagram"></i></p> <p class="mx-2"><i class="bi bi-facebook"></i></p><p class="mx-2"><i class="bi bi-youtube"></i></p><p class="mx-2"><i class="bi bi-discord"></i></p>
