2033f5dc869784638510f4eda70936f26633bfa6
Victor <victor246257@gmail.com>
2024-02-27 14:17:04 +0100

    mod

---
+++ TravelApp/GiuseSolare.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: solare
+*/
+?>
+
+
+<div id='solare'>
+<?php get_header(); ?>
+<style>
+#solare{
+    background: url('<?php echo get_post_meta(get_the_ID(), 'background_image', true); ?>'); 
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: auto;
+}
+
+</style>
+
+<div class="container imageBG">
+  <div class="row d-flex justify-content-center">
+    <div class="col">
+      <?php
+      if ( have_posts() ) : 
+        while ( have_posts() ) : the_post();
+          the_post_thumbnail();
+          the_title('<h1 class="card-body text-center text-white my-3">','</h1>');
+          the_content();
+        endwhile;
+      else :
+        _e('Sorry, no posts matched your criteria.', 'textdomain');
+      endif;
+      ?>
+
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
+</div>
