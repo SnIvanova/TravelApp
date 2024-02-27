2b533edd745ebae272b4f6f76264675905dfa2b9
Xaivar <agifuni92@gmail.com>
2024-02-27 12:34:51 +0100

    Creazione pagina "Offerte speciali"
    Duodenodigiunocefalopancreasectomia

---
+++ TravelApp/offertespeciali.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Offerte Speciali
+*/
+?>
+
+<div id='contenitore'>
+
+    <?php get_header(); ?>
+
+
+    <div class="tab-class text-center">
+        <?php the_content(); ?>
+
+    </div>
+
+    <?php get_footer(); ?>
+</div>
